caeef33ff8859f6af61059c73d1bbc4a410eeccc
jgun0506 <139442762+jgun0506@users.noreply.github.com>
2026-02-20 16:38:57 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 4 ms, Memory: 3256 KB -BaekjoonHub

---
+++ C++17 (Clang)/백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+int dist[200005];
+
+using namespace std;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, k;
+    cin >> n >> k;
+    fill(dist, dist + 200004, 100000);
+    
+    queue <int> Q;
+    Q.push(n);
+    dist[n] = 0;
+
+    while (!Q.empty()) {
+        int cur = Q.front();
+        Q.pop();
+
+        for (int dir : {cur - 1, cur + 1, 2 * cur}) {
+
+            if (dir < 0 || dir > 200000 || dist[dir] <= dist[cur])
+                continue;
+            if (dir == 2 * cur) {
+                if (dist[dir] > dist[cur])
+                    dist[dir] = dist[cur];
+                Q.push(dir);
+                continue;
+            }
+            if (dist[dir] > dist[cur] + 1)
+                dist[dir] = dist[cur] + 1;
+            Q.push(dir);
+        }
+    }
+    cout << dist[k];
+
+    return 0;
+}
