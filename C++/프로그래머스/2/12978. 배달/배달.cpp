5f1e0682abef0719dbf78f3bc2f55980b1ac1b1d
jgun0506 <139442762+jgun0506@users.noreply.github.com>
2026-05-17 15:58:29 +0900

    [level 2] Title: 배달, Time: 0.18 ms, Memory: 4.07 MB -BaekjoonHub

--- C++/프로그래머스/2/12978. 배달/배달.cpp
+++ C++/프로그래머스/2/12978. 배달/배달.cpp
@@ -11,8 +11,8 @@ int solution(int N, vector<vector<int> > road, int K) {
     fill(dist.begin(), dist.begin()+100, inf);
 
     for (auto arr : road) {
-        int a = arr[0];//마을번호1
-        int b = arr[1];//마을번호2
+        int a = arr[0];//인접한 마을 번호
+        int b = arr[1];
         int c = arr[2];//걸리는 시간
 
         G[a].push_back({ c,b });
@@ -20,13 +20,31 @@ int solution(int N, vector<vector<int> > road, int K) {
     }
     priority_queue<pii, vector<pii>, greater<pii>> pq;
     
-    dist[1] = 0;//시작점 설정
+    dist[1] = 0;//시작점 설정해주기
     pq.push({0,1});
     
     while (!pq.empty()) {
         auto [curtime, curnode] = pq.top();
         pq.pop();
         
-        if(dist[curnode]<)
+        if (dist[curnode] < curtime) {
+            continue;
+        }
     
+        for (auto [nexttime, nextnode] : G[curnode]) {
+            auto newtime = curtime + nexttime;
+
+            if (dist[nextnode] > newtime) {
+                 dist[nextnode] = newtime;
+                 pq.push({ newtime, nextnode });
+            }
+        }
+    }
+    int ans=0;
+    for(auto a: dist){
+        if(a<=K){
+            ans++;
+        }
+    }   
+    return ans;
 }
